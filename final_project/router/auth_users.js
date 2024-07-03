c70b90f915e0900558ceea5aa7d844ec435bac3c
Benjamin Hahl <contact@hahl.media>
2024-07-03 17:00:25 +0200

    Task 7 for the Peer Review Assignment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -18,14 +18,47 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-  //returns boolean
-  //write code to check if username and password match the one we have in records.
+  // code to check if username and password match the one we have in records.
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 //only registered users can login
 regd_users.post('/login', (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({ message: 'Username or password is missing' });
+  }
+  //User authentication
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      'access',
+      { expiresIn: 30 * 60 } //Token valid for 30min
+    );
+
+    // Use session storage for token and username
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send(`Hello ${username}, you are logged in`);
+  } else {
+    return res
+      .status(208)
+      .json({ message: 'Invalid Login. Check username and password' });
+  }
 });
 
 // Add a book review
